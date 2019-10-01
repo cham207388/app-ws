2b8ff4b253520fc90c6d0cc1692eea305474e4e6
Alhagie Bai Cham <alhagiebcham.ee@gmail.com>
2019-09-30 22:37:39 -0400

    refactoring

--- src/main/java/com/udemy/appws/security/WebSecurity.java
+++ src/main/java/com/udemy/appws/security/WebSecurity.java
@@ -49,16 +49,7 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
-    /*@Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/h2-console/**").hasRole("ADMIN")//allow h2 console access to admins only
-                .anyRequest().authenticated()//all other urls can be access by any authenticated role
-                .and().formLogin()//enable form login instead of basic login
-                .and().csrf().ignoringAntMatchers("/h2-console/**")//don't apply CSRF protection to /h2-console
-                .and().headers().frameOptions().sameOrigin();//allow use of frame to same origin urls
-    }*/
-
+    
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
